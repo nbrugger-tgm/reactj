da5c9b02d5a6967ba651e4f5088cd3c023b8cd30
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-11-30 09:31:52 +0100

    Beautify Observer

--- src/main/java/com/niton/reactj/Observer.java
+++ src/main/java/com/niton/reactj/Observer.java
@@ -3,14 +3,28 @@ package com.niton.reactj;
 import com.niton.reactj.special.ListActions;
 import com.niton.reactj.special.ReactiveList;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * An Observer is used to observe changes in the state of an Object,
+ * eg. reports changes to the properties
+ *
+ * @param <M> The Model class to observe
+ */
 public abstract class Observer<M extends Reactable> {
-	private final Map<String, Object>                               valueCache      = new HashMap<>();
-	public abstract void onChange(String property,Object value);
-	private       M                                         model;
+	private final     Map<String, Object> valueCache = new ConcurrentHashMap<>();
+	private transient M                   model;
+
+	/**
+	 * Listender-like<br>
+	 * Called when an property of the bound object changes
+	 *
+	 * @param property the name of the changed property as a String
+	 * @param value    the new assigned value
+	 */
+	public abstract void onChange(String property, Object value);
 
 	public M getModel() {
 		return model;
@@ -21,16 +35,20 @@ public abstract class Observer<M extends Reactable> {
 	}
 
 
+	/**
+	 * Updates the cache with the pairs in the map
+	 *
+	 * @param changed the map to put into the cache
+	 */
 	protected void updateCache(Map<String, Object> changed) {
 		valueCache.putAll(changed);
 	}
+
 	/**
 	 * Updates the UI to the new state of the model
 	 */
 	public void update() {
-		Map<String, Object> changed = new HashMap<>();
-		getChanges(changed);
-		update(changed);
+		update(getChanges());
 	}
 
 	/**
@@ -44,14 +62,27 @@ public abstract class Observer<M extends Reactable> {
 		}
 	}
 
-	private void getChanges(Map<String, Object> changed) {
-		Map<String, Object> state = model.getState();
+	/**
+	 * Calculates every property that changed since the last cache update
+	 *
+	 * @return the map of all properties that changed
+	 */
+	private Map<String, Object> getChanges() {
+		Map<String, Object> changed = new ConcurrentHashMap<>();
+		Map<String, Object> state   = model.getState();
 		for (String property : state.keySet()) {
 			detectChange(changed, property, state.get(property));
 		}
+		return changed;
 	}
 
-
+	/**
+	 * Adds the property to the Map if the actual value is different than the one in cache
+	 *
+	 * @param changed      the map to add the change to if necessary
+	 * @param property     the name of the property to check
+	 * @param currentValue the value to compare to the cache
+	 */
 	private void detectChange(Map<String, Object> changed, String property, Object currentValue) {
 		Object oldValue = valueCache.get(property);
 		if (!Objects.equals(currentValue, oldValue)) {
@@ -67,8 +98,12 @@ public abstract class Observer<M extends Reactable> {
 	 * @param model the model to display for this controller (its UI)
 	 */
 	public void bind(M model) {
-		if(model != null)
-			model.unbind(this);
+		if (model == null) {
+			throw new IllegalArgumentException("Cannot bind to null");
+		}
+		if (this.model != null) {
+			this.model.unbind(this);
+		}
 		model.bind(this);
 		this.model = model;
 		if (model instanceof ReactiveList) {
