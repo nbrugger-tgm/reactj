eff5fb749eb29191e53859ca520d71894395718c
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2021-01-07 22:59:44 +0100

    test(test-coverage): add expected error test cases

--- src/test/java/com/niton/reactj/test/AnnotationTest.java
+++ src/test/java/com/niton/reactj/test/AnnotationTest.java
@@ -6,6 +6,7 @@ import com.niton.reactj.ReactiveProxy;
 import com.niton.reactj.annotation.ReactivResolution;
 import com.niton.reactj.annotation.Reactive;
 import com.niton.reactj.annotation.Unreactive;
+import com.niton.reactj.exceptions.ReactiveException;
 import com.niton.reactj.mvc.ReactiveBinder;
 import com.niton.reactj.mvc.ReactiveController;
 import com.niton.reactj.mvc.ReactiveObject;
@@ -85,20 +86,7 @@ public class AnnotationTest {
 		assertEquals(b,bCalled);
 	}
 
-	public static class Base {
-		@Reactive("test")
-		private int a;
-		@Unreactive
-		private int b;
 
-		public void setA(int a) {
-			this.a = a;
-		}
-
-		public void setB(int b) {
-			this.b = b;
-		}
-	}
 
 	@Test
 	void testFlat(){
@@ -114,6 +102,76 @@ public class AnnotationTest {
 		assertTrue(cCalled);
 	}
 
+	@Test
+	void errorTesting(){
+		assertThrows(ReactiveException.class, () -> {
+			ReactiveProxy<FlatBase> flatProxy = ReactiveObject.create(FlatBase.class);
+			FlatBase                flat        = flatProxy.object;
+		});
+
+		assertThrows(ReactiveException.class, () -> {
+			ReactiveProxy<FlatBase> flatProxy = ReactiveObject.create(FlatBase.class,"Wrong type");
+			FlatBase                flat        = flatProxy.object;
+		});
+		assertThrows(ReactiveException.class, () -> {
+			ReactiveComponent<AnnotationTest> deepComponent = new ReactiveComponent<AnnotationTest>() {
+				@Override
+				public void createBindings(ReactiveBinder binder) {}
+
+				@Override
+				public void registerListeners(AnnotationTest controller) {}
+
+				@Reactive("a")
+				void wrong(int too,int many){}
+			};
+		});
+		assertThrows(ReactiveException.class, () -> {
+			ReactiveComponent<AnnotationTest> deepComponent = new ReactiveComponent<AnnotationTest>() {
+				@Override
+				public void createBindings(ReactiveBinder binder) {}
+
+				@Override
+				public void registerListeners(AnnotationTest controller) {}
+
+				@Reactive("test")
+				void wrong(String badArgument){}
+			};
+			ReactiveController<AnnotationTest,ReactiveProxy<DeepBase>> cont = new ReactiveController<>(deepComponent,this);
+			cont.bind(deepProxy);
+		});
+
+		assertThrows(ReactiveException.class, () -> {
+			ReactiveComponent<AnnotationTest> deepComponent = new ReactiveComponent<AnnotationTest>() {
+				@Override
+				public void createBindings(ReactiveBinder binder) {}
+
+				@Override
+				public void registerListeners(AnnotationTest controller) {}
+
+				@Reactive("test")
+				void errorProne(int value){
+					throw new NullPointerException("Intentional error");
+				}
+			};
+			ReactiveController<AnnotationTest,ReactiveProxy<DeepBase>> cont = new ReactiveController<>(deepComponent,this);
+			cont.bind(deepProxy);
+		});
+
+	}
+	public static class Base {
+		@Reactive("test")
+		private int a;
+		@Unreactive
+		private int b;
+
+		public void setA(int a) {
+			this.a = a;
+		}
+
+		public void setB(int b) {
+			this.b = b;
+		}
+	}
 	@ReactivResolution(DEEP)
 	public static class DeepBase extends Base {
 		private int c;
@@ -130,5 +188,10 @@ public class AnnotationTest {
 		public void setC(int c) {
 			this.c = c;
 		}
+	}
+
+	public static class FailBase extends Base {
+		public int c;
+		private FailBase(int wröng){};
 	}
 }
