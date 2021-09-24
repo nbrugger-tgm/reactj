53c4e47da21a67203fd4e34c9bc1c07b04d497a3
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-09-24 23:37:00 +0200

    fix: remove deprecated dependency

--- lists/src/main/java/module-info.java
+++ lists/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module reactj.lists {
 	exports com.niton.reactj.api.lists;
 	exports com.niton.reactj.lists.diff;
-	exports com.niton.reactj.lists.observer;
 	requires reactj.utils;
 	requires static org.junit.jupiter.api;
 	requires reactj.observer;
